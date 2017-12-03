9fda7bb03fb16825e90d9cfafa30f8fd58d9f42d
LEOMARAAC\Leomara <leomara.andrade@gmail.com>
2017-12-03 16:20:38 -0200

    Adicionando uma verificação se o arquivo existe antes de abri-lo

--- Loja/Arquivos/LerArquivo.php
+++ Loja/Arquivos/LerArquivo.php
@@ -9,7 +9,13 @@ require_once '../../constante.php';
 function lerDestaque()
 {
     $caminho = HOME_PATH.'/Txt/destaque.txt';
-    $conteudo = file_get_contents($caminho,"r");
+    if(file_exists($caminho))
+    {
+        $conteudo = file_get_contents($caminho,"r");
+    }
+    else{
+        return false;
+    }
     return $conteudo;
 }
 function lerCarrossel()
@@ -23,17 +29,28 @@ function lerCarrossel()
     $arquivo = fopen($caminho,"r");
     $cont = 0;
 
-    while(! feof($arquivo)) //Enquanto não chegar no fim fim do arquivo
+    while(! feof($arquivo) && $cont < 3) //Enquanto não chegar no fim fim do arquivo
     {
         $ids[$cont] = fgets($arquivo);
         $cont++;
     }
     fclose($arquivo);
-    return$ids;
+    return $ids;
 }
 function lerInfo($tipo)
 {
     $caminho = HOME_PATH.'/Txt/'.$tipo.'.txt';
+    if (!file_exists($caminho)) {
+        return false;
+    }
     $conteudo = file_get_contents($caminho,"r");
     return $conteudo;
+}
+function getCoordenadas(){
+    $caminho = HOME_PATH.'/Txt/coordenadas.ini';
+    if (!file_exists($caminho)) {
+        return false;
+    }
+    $coodenadas = parse_ini_file($caminho);
+    return $coodenadas;
 }
